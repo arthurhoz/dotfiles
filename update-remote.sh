73d10ae8a1ec22be5ec15dddb35da2118eb676e7
Arthur Damasceno <damascenoarthur653@gmail.com>
2026-07-27 15:57:40 -0300

    feat: add scripts and new configs

---
+++ update-remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsync -av ~/.config/hypr/ .config/hypr/
+rsync -av ~/.config/alacritty/ .config/alacritty/
+rsync -av ~/.config/rofi/ .config/rofi/
+rsync -av ~/.config/waybar/ .config/waybar/
